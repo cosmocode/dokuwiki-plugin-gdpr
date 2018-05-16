0bc0c406d4130729fba86180af4aca04566ec764
Michael Große <grosse@cosmocode.de>
2018-05-16 16:18:36 +0200

    test: Add tests for media changelog cleaning ✅

---
+++ _test/cleaningMedia.test.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * General tests for the cleanoldips plugin
+ *
+ * @group plugin_cleanoldips
+ * @group plugins
+ */
+class cleaningMedia_plugin_cleanoldips_test extends DokuWikiTest
+{
+    protected $pluginsEnabled = ['cleanoldips'];
+
+    protected $yesterday;
+
+    protected $mediaID = 'galaxy:andromeda.jpg';
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $changelogFN = mediaMetaFN($this->mediaID, '.changes');
+        io_makeFileDir($changelogFN);
+        $this->yesterday = time() - 60 * 60 * 24;
+        file_put_contents($changelogFN, '1522767335	192.168.0.105	C	galaxy:andromeda.jpg		created		36
+1522767349	192.168.0.105	E	galaxy:andromeda.jpg				12
+1523956708	192.168.0.105	E	galaxy:andromeda.jpg	admin			23
+1524145287	192.168.0.105	E	galaxy:andromeda.jpg	admin			19
+1524464616	192.168.0.105	E	galaxy:andromeda.jpg	admin	ok		0
+');
+        $handle = fopen($changelogFN, 'ab');
+        $recentChangelogLine = $this->yesterday . "	192.168.0.105	E	galaxy:andromeda.jpg	admin	ok		0\n";
+        fwrite($handle, $recentChangelogLine);
+        fclose($handle);
+    }
+
+    public function testCleaningChangelog()
+    {
+        $eventData = [
+            'isMedia' => true,
+            'trimmedChangelogLines' => [],
+            'removedChangelogLines' => ['1526477811	192.168.0.105	C	galaxy:andromeda.jpg	admin	created		1689508'],
+        ];
+
+        trigger_event('TASK_RECENTCHANGES_TRIM', $eventData);
+        $actualChangelogContent = file_get_contents(mediaMetaFN($this->mediaID, '.changes'));
+
+        $expectedChangelogContent = '1522767335	             	C	galaxy:andromeda.jpg		created		36
+1522767349	             	E	galaxy:andromeda.jpg				12
+1523956708	             	E	galaxy:andromeda.jpg	admin			23
+1524145287	             	E	galaxy:andromeda.jpg	admin			19
+1524464616	             	E	galaxy:andromeda.jpg	admin	ok		0
+' . $this->yesterday . "	192.168.0.105	E	galaxy:andromeda.jpg	admin	ok		0\n";
+        $this->assertEquals($expectedChangelogContent, $actualChangelogContent);
+
+    }
+}
